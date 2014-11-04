76572964c87cba6f3d03f0ec614f2bbd2c755151
Seth Broweleit <hello[@]getsetbro[.]com>
2014-11-04 15:34:45 -0600

    vGettingClose

---
+++ README.md
@@ -0,0 +1,7 @@
+
+I really don't want to use an actual TABLE for this site nav but there is no other way to get the spacing that is required from the PSD file. I mean I coould use display:table in CSS if this page didnt need to support oldIE. The customer won't care what the code looks like.
+The other option shown in the PSD and give all the nav elements the same percent of the site width which is around 16% for this number of nav items. But that doesnt perform as well in RWD as the TABLE layout.
+
+
+I am not saying there is no way to make this background image centered and scrollable and always fill the screen and work in oldIE but I am saying there is no GOOD WAY of doing it. It is not worth it. I am going with progressive enhancement, the newer browsers get the best experience.
+
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,45 +1,47 @@
-//$('.lt-ie9 .clearfix').append('<div class="clearfixer"></div>');
-
-var $carousel = $('#carousel');
-var id = 1;
-var itemsAmount = 4;
-var delay = 5000;
-var shouldContinue = true;
-var startTime;
-var timer;
-var moveCarousel  = function(itm){
+(function(){
   'use strict';
-  $carousel.attr('class','ch'+itm);
-  if(shouldContinue){
-    timer = setTimeout(startTime, delay);
-  }
-};
+  var $carousel = $('#carousel');
+  var $carouselItem = $carousel.find('.carousel-item');
+  var id = 1;
+  var itemsAmount = 4;
+  var delay = 5000;
+  var shouldContinue = true;
+  var startTime;
+  var timer;
 
-startTime = function(){
-  'use strict';
-  if(id < itemsAmount){
-      id++;
+  var moveCarousel  = function(itm){
+    $carousel.attr('class','ch'+itm);
+    if(shouldContinue){
+      timer = setTimeout(startTime, delay);
     }
-  else{
-    id = 1;
-  }
-  moveCarousel(id);
-};
+  };
 
-timer = setTimeout(startTime, delay);
+  startTime = function(){
+    if(id < itemsAmount){
+        id++;
+      }
+    else{
+      id = 1;
+    }
+    moveCarousel(id);
+  };
 
-$('.carousel-item').on('click', function(){
-  'use strict';
-  id = this.id.split('_')[1];
-  clearTimeout(timer);
-  shouldContinue = false;
-  moveCarousel(id);
-});
+  timer = setTimeout(startTime, delay);
 
-$carousel.on('mouseleave', function(){
-  'use strict';
-  if( !shouldContinue ){
-    shouldContinue = true;
-    timer = setTimeout(startTime, delay);
-  }
-});
+  $carouselItem.on('click', function(){
+    id = this.id.split('_')[1];
+    clearTimeout(timer);
+    shouldContinue = false;
+    moveCarousel(id);
+  });
+
+  $carousel.on('mouseleave', function(){
+    if( !shouldContinue ){
+      shouldContinue = true;
+      timer = setTimeout(startTime, delay);
+    }
+  });
+
+
+
+})();
