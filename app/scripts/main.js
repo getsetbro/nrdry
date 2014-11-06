f5a343815130f0dc216b8d0ee47714e637b7d9d2
Seth Broweleit <hello[@]getsetbro[.]com>
2014-11-06 14:03:33 -0600

    ie7 grid js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -44,7 +44,7 @@
 
 
   if( ($('html').hasClass('lt-ie9')) ){
-      $('.grid70, .grid30, .grid33').each(function(){
+      $('.grid70, .grid30, .grid33, .grid27, .grid72, .carousel-item').each(function(){
           var fullW = $(this).outerWidth(),
               actualW = $(this).width(),
               wDiff = fullW - actualW,
