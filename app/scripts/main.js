4ee0584956cf0c66df0e48eb3df46c9182ce31ce
Seth Broweleit <hello[@]getsetbro[.]com>
2014-11-12 11:15:03 -0600

    bg js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,66 +1,80 @@
-(function(){
-  'use strict';
-  var $carousel = $('#carousel');
-  var $carouselItem = $carousel.find('.carousel-item');
-  var id = 1;
-  var itemsAmount = 4;
-  var delay = 5000;
-  var shouldContinue = true;
-  var startTime;
-  var timer;
+(function() {
+    'use strict';
+    var $documentBody = $(document.body);
+    var $bgw = $documentBody.find('#bgw');
+    var $bgImg = $documentBody.find('#bgImg');
+    var $contain = $documentBody.find('.contain');
+    var $carousel = $documentBody.find('#carousel');
+    var $carouselItem = $carousel.find('.carousel-item');
+    var id = 1;
+    var itemsAmount = 4;
+    var delay = 5000;
+    var shouldContinue = true;
+    var startTime;
+    var timer;
 
-  var moveCarousel  = function(itm){
-    $carousel.attr('class','ch'+itm);
-    if(shouldContinue){
-      timer = setTimeout(startTime, delay);
+    var moveCarousel = function(itm) {
+        $carousel.attr('class', 'ch' + itm);
+        if (shouldContinue) {
+            timer = setTimeout(startTime, delay);
+        }
+    };
+
+    startTime = function() {
+        if (id < itemsAmount) {
+            id++;
+        } else {
+            id = 1;
+        }
+        moveCarousel(id);
+    };
+
+    timer = setTimeout(startTime, delay);
+
+    $carouselItem.on('click', function() {
+        id = this.id.split('_')[1];
+        clearTimeout(timer);
+        shouldContinue = false;
+        moveCarousel(id);
+    });
+
+    $carousel.on('mouseleave', function() {
+        if (!shouldContinue) {
+            shouldContinue = true;
+            timer = setTimeout(startTime, delay);
+        }
+    });
+
+
+    if (($documentBody.hasClass('lt-ie9'))) {
+        $('.grid27, .grid30, .grid70, .grid72, .grid100, .footer').each(function() {
+            var fullW = $(this).outerWidth(),
+                actualW = $(this).width(),
+                wDiff = fullW - actualW,
+                newW = actualW - wDiff;
+
+            $(this).css('width', newW);
+        });
+        $('.grid33, .carousel-item').each(function() {
+            var fullW = $(this).outerWidth(),
+                actualW = $(this).width(),
+                wDiff = fullW - actualW,
+                newW = actualW - wDiff;
+
+            $(this).css('width', newW - 1);
+        });
     }
-  };
 
-  startTime = function(){
-    if(id < itemsAmount){
-        id++;
-      }
-    else{
-      id = 1;
+//set the background size on page load
+    if ($documentBody.width() < $documentBody.height()) {
+        $bgImg.height($contain.height());
+        $bgImg.width('auto');
+        $bgw.height('auto');
+    } else {
+        $bgImg.height('auto');
+        $bgImg.width($documentBody.width());
+        $bgw.height($contain.height());
     }
-    moveCarousel(id);
-  };
-
-  timer = setTimeout(startTime, delay);
-
-  $carouselItem.on('click', function(){
-    id = this.id.split('_')[1];
-    clearTimeout(timer);
-    shouldContinue = false;
-    moveCarousel(id);
-  });
-
-  $carousel.on('mouseleave', function(){
-    if( !shouldContinue ){
-      shouldContinue = true;
-      timer = setTimeout(startTime, delay);
-    }
-  });
-
-
-  if( ($(document.body).hasClass('lt-ie9')) ){
-      $('.grid27, .grid30, .grid70, .grid72, .grid100, .footer').each(function(){
-          var fullW = $(this).outerWidth(),
-              actualW = $(this).width(),
-              wDiff = fullW - actualW,
-              newW = actualW - wDiff;
-
-          $(this).css('width',newW);
-      });
-      $('.grid33, .carousel-item').each(function(){
-          var fullW = $(this).outerWidth(),
-              actualW = $(this).width(),
-              wDiff = fullW - actualW,
-              newW = actualW - wDiff;
-
-          $(this).css('width', newW - 1 );
-      });
-  }
 
 })();
 
