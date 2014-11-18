641e7eefc7560c861bd4949e0524f4b3090716eb
Seth Broweleit <hello[@]getsetbro[.]com>
2014-11-18 11:46:03 -0600

    site v1

---
+++ site/scripts/df6deff0.main.js
@@ -0,0 +1,98 @@
+(function() {
+    'use strict';
+    var $documentBody = $(document.body);
+    var $bgw = $documentBody.find('#bgw');
+    var $bgImg = $documentBody.find('#bgImg');
+    var $contain = $documentBody.find('.contain');
+    var $carousel = $documentBody.find('#carousel');
+    var $carouselItem = $carousel.find('.carousel-item');
+    var id = 1;
+    var itemsAmount = 4;
+    var delay = 5000;
+    var shouldContinue = true;
+    var startTime;
+    var moveCarousel;
+    var timer;
+
+  //carousel
+    moveCarousel = function(itm) {
+        $carousel.attr('class', 'ch' + itm);
+        if (shouldContinue) {
+            timer = setTimeout(startTime, delay);
+        }
+    };
+
+    startTime = function() {
+        if (id < itemsAmount) {
+            id++;
+        } else {
+            id = 1;
+        }
+        moveCarousel(id);
+    };
+
+    timer = setTimeout(startTime, delay);
+
+    $carouselItem.on('click', function() {
+        id = this.id.split('_')[1];
+        clearTimeout(timer);
+        shouldContinue = false;
+        moveCarousel(id);
+    });
+
+    $carousel.on('mouseleave', function() {
+        if (!shouldContinue) {
+            shouldContinue = true;
+            timer = setTimeout(startTime, delay);
+        }
+    });
+  //end carousel
+
+  //set sizes for old IE
+    if ( $documentBody.hasClass('lt-ie8') ) {
+        $('.grid27, .grid30, .grid70, .grid72, .grid100').each(function() {
+            var fullW = $(this).outerWidth(),
+                actualW = $(this).width(),
+                wDiff = fullW - actualW,
+                newW = actualW - wDiff;
+
+            $(this).css('width', newW);
+        });
+        $('.grid33, .carousel-item').each(function() {
+            var fullW = $(this).outerWidth(),
+                actualW = $(this).width(),
+                wDiff = fullW - actualW,
+                newW = actualW - wDiff;
+
+            $(this).css('width', newW - 1);
+        });
+    }
+
+
+$(window).load(function(){
+     //initialize after images are loaded
+    //set the background size on page load
+    if ($documentBody.width() < $documentBody.height()) {
+        $bgImg.height($contain.height());
+        $bgImg.width('auto');
+        $bgw.height('auto');
+    } else {
+        $bgImg.height('auto');
+        $bgImg.width($documentBody.width());
+        $bgw.height($contain.height());
+    }
+});
+})();
+
+//twitter widget
+(function(d, s, id) {
+    'use strict';
+    var js,
+        fjs = d.getElementsByTagName(s)[0];
+    if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }
+})(document, 'script', 'twitter-wjs');
